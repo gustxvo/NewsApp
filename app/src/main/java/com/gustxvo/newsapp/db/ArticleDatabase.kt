6f823e14633194f0e74adf50e33dbd7dd89549cb
Gustavo Almeida <gualmeida2004@gmail.com>
2022-12-06 02:10:34 -0300

    Setup database and type converters

---
+++ app/src/main/java/com/gustxvo/newsapp/db/ArticleDatabase.kt
@@ -0,0 +1,32 @@
+package com.gustxvo.newsapp.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.gustxvo.newsapp.model.Article
+
+@TypeConverters(Converters::class)
+@Database(entities = [Article::class], version = 1)
+abstract class ArticleDatabase : RoomDatabase() {
+
+    abstract fun articleDao(): ArticleDao
+
+    companion object {
+        @Volatile
+        private var instance: ArticleDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: createDatabase(context).also { instance = it }
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                ArticleDatabase::class.java,
+                "article_db"
+            ).build()
+    }
+}

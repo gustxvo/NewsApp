f6cc63e271028ae5ce7770b6e7a5dbfec28d7acc
Gustavo Almeida <gualmeida2004@gmail.com>
2022-12-06 13:47:22 -0300

    Get network response

--- app/src/main/java/com/gustxvo/newsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/gustxvo/newsapp/repository/NewsRepository.kt
@@ -1,8 +1,10 @@
 package com.gustxvo.newsapp.repository
 
+import com.gustxvo.newsapp.api.RetrofitInstance
 import com.gustxvo.newsapp.db.ArticleDatabase
 
 class NewsRepository(val database: ArticleDatabase) {
 
-
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.newsApi.getBreakingNews(countryCode, pageNumber)
 }
--- app/src/main/java/com/gustxvo/newsapp/ui/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/gustxvo/newsapp/ui/viewmodel/NewsViewModel.kt
@@ -1,11 +1,39 @@
 package com.gustxvo.newsapp.ui.viewmodel
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
+import com.gustxvo.newsapp.model.NewsResponse
 import com.gustxvo.newsapp.repository.NewsRepository
+import com.gustxvo.newsapp.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
-class NewsViewModel(val newsRepository: NewsRepository) : ViewModel() {
+class NewsViewModel(private val newsRepository: NewsRepository) : ViewModel() {
 
+    private val _breakingNews = MutableLiveData<Resource<NewsResponse>>()
+
+    val breakingNews: LiveData<Resource<NewsResponse>> = _breakingNews
+
+    private var breakingNewsPage = MutableLiveData<Int>()
+
+    fun getBreakingNews(countryCode: String) {
+        viewModelScope.launch {
+            _breakingNews.postValue(Resource.Loading())
+
+            val response = newsRepository.getBreakingNews(
+                countryCode, breakingNewsPage.value ?: 1
+            )
+            _breakingNews.postValue(handleBreakingNewsResponse(response))
+        }
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
 
 class NewsViewModelFactory(private val newsRepository: NewsRepository) : ViewModelProvider.Factory {
